e59eaab471aa1e4986bf7201e1a3ccd303ec13d0
skmitra <mitrakumarsujan@gmail.com>
2021-09-30 00:48:12 +0530

    added custom error handler to maintain uniform error messages

---
+++ notification-service/src/main/java/com/github/sujankumarmitra/notificationservice/v1/controller/JsonViewErrorWebExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.github.sujankumarmitra.notificationservice.v1.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.sujankumarmitra.notificationservice.v1.controller.dto.ErrorResponse;
+import com.github.sujankumarmitra.notificationservice.v1.exception.DefaultErrorDetails;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.SynchronousSink;
+
+import java.util.List;
+
+/**
+ * @author skmitra
+ * @since Sep 30/09/21, 2021
+ */
+@Component
+@AllArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class JsonViewErrorWebExceptionHandler implements ErrorWebExceptionHandler {
+
+    private final ObjectMapper mapper;
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        ErrorResponse errorResponse = buildErrorResponse(ex);
+        ServerHttpResponse httpResponse = exchange.getResponse();
+
+        Mono<DataBuffer> dataBufferMono = Mono.just(errorResponse)
+                .handle(this::convertToBytes)
+                .map(httpResponse.bufferFactory()::wrap);
+
+
+        return httpResponse.writeWith(dataBufferMono);
+    }
+
+    private void convertToBytes(ErrorResponse res, SynchronousSink<byte[]> sink) {
+        try {
+            byte[] bytes = mapper.writeValueAsBytes(res);
+            sink.next(bytes);
+        } catch (JsonProcessingException e) {
+            sink.error(e);
+        }
+    }
+
+    private ErrorResponse buildErrorResponse(Throwable ex) {
+        return new ErrorResponse(List.of(new DefaultErrorDetails(ex.getMessage())));
+    }
+}
