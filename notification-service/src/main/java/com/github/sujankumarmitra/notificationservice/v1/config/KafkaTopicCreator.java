c73b39a52f2479d78963e601d6585fdaec841400
skmitra <mitrakumarsujan@gmail.com>
2021-11-12 19:33:46 +0530

    added auto topic creation

---
+++ notification-service/src/main/java/com/github/sujankumarmitra/notificationservice/v1/config/KafkaTopicCreator.java
@@ -0,0 +1,51 @@
+package com.github.sujankumarmitra.notificationservice.v1.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Properties;
+
+import static java.util.Optional.empty;
+
+/**
+ * Creates Kafka topics for notifications if not created already
+ *
+ * @author skmitra
+ * @since Nov 12/11/21, 2021
+ */
+@Component
+@AllArgsConstructor
+@Slf4j
+public class KafkaTopicCreator implements InitializingBean {
+
+    private final KafkaProperties kafkaProperties;
+
+    private void logResult(Void __, Throwable th) {
+        if (th != null) log.warn("Failed to create notification topic", th);
+        else log.info("Notification topics created");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Properties adminClientProps = new Properties();
+        adminClientProps.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaProperties.getBootstrapServers());
+
+        AdminClient adminClient = AdminClient.create(adminClientProps);
+        NewTopic notificationTopic = new NewTopic(
+                kafkaProperties.getNotificationTopicName(),
+                empty(), empty());
+
+        adminClient.createTopics(List.of(notificationTopic))
+                .all()
+                .whenComplete(this::logResult);
+
+        adminClient.close();
+    }
+}
