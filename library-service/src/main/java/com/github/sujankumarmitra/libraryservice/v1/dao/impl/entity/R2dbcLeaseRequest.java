d0c20934f10baf7416dbe3b3c6138dd2fa98c784
skmitra <mitrakumarsujan@gmail.com>
2021-12-07 23:51:10 +0530

    added LeaseRequestDao impl

--- library-service/src/main/java/com/github/sujankumarmitra/libraryservice/v1/dao/impl/entity/R2dbcLeaseRequest.java
+++ library-service/src/main/java/com/github/sujankumarmitra/libraryservice/v1/dao/impl/entity/R2dbcLeaseRequest.java
@@ -4,7 +4,12 @@ import com.github.sujankumarmitra.libraryservice.v1.model.LeaseRequest;
 import com.github.sujankumarmitra.libraryservice.v1.model.LeaseStatus;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.Objects;
 import java.util.UUID;
 
 import static com.github.sujankumarmitra.libraryservice.v1.util.StringUtil.nullableToString;
@@ -15,13 +20,18 @@ import static com.github.sujankumarmitra.libraryservice.v1.util.StringUtil.nulla
  */
 @Getter
 @Setter
+@Table("lease_requests")
 public final class R2dbcLeaseRequest extends LeaseRequest {
+    @Id
     private UUID id;
+    @Column("book_id")
     private UUID bookId;
+    @Column("user_id")
     private String userId;
     private LeaseStatus status;
     private Long timestamp;
 
+    @Transient
     public UUID getUuid() {
         return id;
     }
@@ -34,7 +44,19 @@ public final class R2dbcLeaseRequest extends LeaseRequest {
         return nullableToString(bookId);
     }
 
+    @Transient
     public UUID getBookUuid() {
         return bookId;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
