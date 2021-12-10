8149a380bea6af597accef9024392675bc47b112
skmitra <mitrakumarsujan@gmail.com>
2021-12-10 19:37:31 +0530

    removed useless classes

--- library-service/src/main/java/com/github/sujankumarmitra/libraryservice/v1/config/ServiceRegistry.java
+++
@@ -1,9 +0,0 @@
-package com.github.sujankumarmitra.libraryservice.v1.config;
-
-/**
- * @author skmitra
- * @since Dec 10/12/21, 2021
- */
-public abstract class ServiceRegistry {
-    public abstract RemoteService getService(String serviceId);
-}
