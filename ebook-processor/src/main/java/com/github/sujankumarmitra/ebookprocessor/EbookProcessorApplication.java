dfadb064b1e728f046c8b51791e94938d3f253e5
skmitra <mitrakumarsujan@gmail.com>
2021-12-11 07:56:52 +0530

    added ebook-processor

---
+++ ebook-processor/src/main/java/com/github/sujankumarmitra/ebookprocessor/EbookProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.github.sujankumarmitra.ebookprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookProcessorApplication.class, args);
+	}
+
+}
---
+++ ebook-processor/src/test/java/com/github/sujankumarmitra/ebookprocessor/EbookProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sujankumarmitra.ebookprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
