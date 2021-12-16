595a463a35531bbd823be913a963ed02c8369bba
skmitra <mitrakumarsujan@gmail.com>
2021-12-16 09:31:39 +0530

    Added build process independent of Maven and JDK
    The Maven build process now happens inside a maven docker container, so the host machine doesn't need to have
    either Maven or JDK installed.

--- notification-service/build.sh
+++ notification-service/build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# builds the project without running tests and builds a docker image
-
-#mvn clean package # to run tests before build
-mvn clean package -DskipTests
+docker volume create e-library_maven_repo
+docker run -it --rm -v e-library_maven_repo:/root/.m2  -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven mvn package -DskipTests
 docker build -t notification-service:1.0.0 .
 docker image prune -f
